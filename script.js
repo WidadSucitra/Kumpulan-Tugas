529ddefd252a16749f06a4f101ad756a6cd856d6
Andi WIdad Sucitra <sucitraandiwidad@gmail.com>
2020-09-03 13:56:09 +0800

    modified

--- script.js
+++ script.js
@@ -12,21 +12,3 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
-
-
-
-var mybutton = document.getElementById("myBtn");
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
